aa82a6e22ab0ca60a19b2e022648316d3441a036
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-18 16:13:44 -0500

    Create turn method assigning cardinal direction

--- mars_rover.rb
+++ mars_rover.rb
@@ -8,4 +8,58 @@ class Rover
     @direction = direction
   end
 
+  def instruction
+  end
+
+  def move
+  end
+
+  def turn(new_direction)
+
+      case
+        when @direction == 'N'
+          if new_direction == 'L'
+            @direction = 'W'
+          else
+            @direction = 'E'
+          end
+        when @direction == 'E'
+          if new_direction == 'L'
+            @direction = 'N'
+          else
+            @direction = 'S'
+          end
+        when @direction == 'S'
+          if new_direction == 'L'
+            @direction = 'E'
+          else
+            @direction = 'W'
+          end
+        when @direction == 'W'
+          if new_direction == 'L'
+            @direction = 'S'
+          else
+            @direction = 'S'
+          end
+      end
+
+  end
+
 end
+
+test = Rover.new(0,0,'N')
+
+test.turn('R')
+puts test.direction
+
+test.turn('L')
+puts test.direction
+
+test.turn('L')
+puts test.direction
+
+
+# N = y + 1
+# E = x + 1
+# S = y - 1
+# W = x - 1
